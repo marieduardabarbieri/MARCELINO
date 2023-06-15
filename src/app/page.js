b2af1ffbd6ad79ce61cf3bfe43a743c0b06f3e77
Luiz F. Picolo <maria.barbieri@estudante.ifms.edu.br>
2023-06-15 11:37:16 -0400

    parte 2 da estilização da página principal

--- src/app/page.js
+++ src/app/page.js
@@ -9,21 +9,23 @@ export default async function Home() {
   const produtos = await req.json();
 
   return (
-    <main className=''>
-      <Link className='LoginAhref2' href="/cadastro" > CADASTRAR </Link>
+    
+    <main className='tudo'>
+     <p> <Link className='LoginAhref2' href="/cadastro" > CADASTRAR </Link></p>
 
-       
+   <div className='Produto1'> 
       {produtos.map(produto => (
-        <div key={produto.codigo}>
-          <p>Produto: {produto.titulo}</p>
-          <p>Data de inserção: {produto.data}</p>
-          <p>Preço: {produto.preco}</p>
-          <p>Descricao: {produto.descricao}</p>
-          <img src={produto.img}></img>
+        <div key={produto.codigo} className='Produto3'>
+          <img src={produto.img}></img><br/><hr/>
+          <p className='Produto2'>Produto: {produto.titulo}</p>
+          <p className='Produto2'>Data de inserção: {produto.data}</p>
+          <p className='Produto2'>Preço: R${produto.preco}</p>
+          <p className='Produto2'>Descricao: {produto.descricao}</p>
           <Link className='LoginAhref' href={`/produto/${produto.codigo}`}>Ver Mais</Link>
         </div>
-      
+  
       ))}
+      </div>
     </main>
   )
 }
